edf019eb1381f91d76773aff17624a78aad351a5
T R Methu <fmethubd@gmail.com>
2023-05-26 23:20:08 +0600

    doctor card design done

--- src/Pages/Home/OurDoctor/Doctor.jsx
+++ src/Pages/Home/OurDoctor/Doctor.jsx
@@ -1,11 +1,12 @@
 import { Rating } from "@smastrom/react-rating";
 import { Link } from "react-router-dom";
+import { ClockIcon, MapPinIcon, CurrencyDollarIcon, BriefcaseIcon } from '@heroicons/react/24/solid'
 
 
 const Doctor = ({doc}) => {
     const {_id, name, address, available_hours, consultation_charge, designation, experience, photo_link, rating} = doc;
     return (
-        <div className="p-10 border">
+        <div className="p-5 border">
             <div className="">
             <img className="md:w-[350px] md:h-[260px] rounded-lg" src={photo_link} alt="" />
             </div>
@@ -17,10 +18,13 @@ const Doctor = ({doc}) => {
                 readOnly
             />
             <hr className="py-1 mt-1" />
-            <p>{address}</p>
-            <p>{available_hours}</p>
-            <p>{consultation_charge}</p>
-            <Link to={`/doctorProfile/${_id}`} className="btn btn-outline">View Profile</Link>
+            <div className="text-sm">
+            <p className="flex py-1"> <MapPinIcon className="h-5 w-5 text-gray-500 mr-3" /> {address}</p>
+            <p className="flex py-1"> <ClockIcon className="h-5 w-5 text-gray-500 mr-3" /> {available_hours}</p>
+            <p className="flex py-1"> <CurrencyDollarIcon className="h-5 w-5 text-gray-500 mr-3" />{consultation_charge}</p>
+            <p className="flex py-1"> <BriefcaseIcon className="h-5 w-5 text-gray-500 mr-3" /> {experience} experienced.</p>
+            </div>
+            <Link to={`/doctorProfile/${_id}`} className="btn btn-outline my-5">View Profile</Link>
         </div>
     );
 };
